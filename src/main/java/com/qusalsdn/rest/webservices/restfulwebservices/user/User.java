90168501e02e6d03066fd0a9384ef501ee775da6
변민우 <yja05092@gmail.com>
2024-04-08 18:44:51 +0900

    User 엔터티 및 테스트 데이터 생성

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,18 @@
 package com.qusalsdn.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "이름의 최소 2자리 이상이여야 합니다.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id, name, birth_date) values(10001, 'byun', current_date());
+insert into user_details(id, name, birth_date) values(10002, 'min', current_date());
+insert into user_details(id, name, birth_date) values(10003, 'woo', current_date());
