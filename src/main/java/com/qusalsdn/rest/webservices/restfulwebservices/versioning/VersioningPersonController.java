04dc9d32998c5e4f6fddcbf35d4f439907e00986
변민우 <yja05092@gmail.com>
2024-04-08 16:11:59 +0900

    REST API 버전 관리 - 요청 매개변수, 헤더, 콘텐츠 협상

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -14,4 +14,34 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersioningOfPerson() {
         return new PersonV2(new Name("byun", "min"));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersioningOfPersonRequestParameter() {
+        return new PersonV1("qusalsdn");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersioningOfPersonRequestParameter() {
+        return new PersonV2(new Name("byun", "min"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersioningOfPersonRequestHeader() {
+        return new PersonV1("qusalsdn");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersioningOfPersonRequestHeader() {
+        return new PersonV2(new Name("byun", "min"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersioningOfPersonAcceptHeader() {
+        return new PersonV1("qusalsdn");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersioningOfPersonAcceptHeader() {
+        return new PersonV2(new Name("byun", "min"));
+    }
 }
