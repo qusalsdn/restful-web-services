e29bb189fd2ba6887cc77f17c05a6ac50461a12d
변민우 <yja05092@gmail.com>
2024-04-08 15:29:45 +0900

    REST API 국제화

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.qusalsdn.rest.webservices.restfulwebservices.helloWorld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World~!";
@@ -19,5 +30,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) { // @PathVariable 어노테이션으로 인해 Spring MVC가 name을 매핑한다.
         return new HelloWorldBean(String.format("Hello World~! %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale local = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", local);
     }
 }
