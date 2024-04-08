027ba3550cc1d246fa940e99a722caeedb812ad1
변민우 <yja05092@gmail.com>
2024-04-08 22:40:01 +0900

    Update UserJpaResource.java

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/user/UserJpaResource.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -18,23 +18,23 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserJpaResource {
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     private PostRepository postRepository;
 
-    public UserJpaResource(UserRepository repository, PostRepository postRepository) {
-        this.repository = repository;
+    public UserJpaResource(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
         this.postRepository = postRepository;
     }
 
     @GetMapping(path = "/jpa/users")
     public List<User> retrieveAllUsers() {
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
     @GetMapping(path = "/jpa/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        Optional<User> user = repository.findById(id);
+        Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) throw new UserNotFoundException("id: " + id);
         EntityModel<User> entityModel = EntityModel.of(user.get());
         WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
@@ -44,7 +44,7 @@ public class UserJpaResource {
 
     @PostMapping(path = "/jpa/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) { // @Valid 어노테이션으로 유효성 검증을 수행할 수 있다.
-        User savedUser = repository.save(user);
+        User savedUser = userRepository.save(user);
         // ServletUriComponentsBuilder.fromCurrentRequest(): 현재 URL, .path("/{id}"): 현재 URL에 "/{id}" 추가, .buildAndExpand(savedUser.getId():id를 생성된 사용자의 아이디로 변경, .toUri(): URL로 변환하여 반환
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build(); // 201을 반환
@@ -52,19 +52,19 @@ public class UserJpaResource {
 
     @DeleteMapping(path = "/jpa/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        repository.deleteById(id);
+        userRepository.deleteById(id);
     }
 
     @GetMapping(path = "/jpa/users/{id}/posts")
     public List<Post> retrievePostsForUser(@PathVariable int id) {
-        Optional<User> user = repository.findById(id);
+        Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) throw new UserNotFoundException("id: " + id);
         return user.get().getPosts();
     }
 
     @PostMapping(path = "/jpa/users/{id}/posts")
     public ResponseEntity<Object> createPostsForUser(@PathVariable int id, @Valid @RequestBody Post post) {
-        Optional<User> user = repository.findById(id);
+        Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) throw new UserNotFoundException("id: " + id);
         post.setUser(user.get());
         Post savedPost = postRepository.save(post);
