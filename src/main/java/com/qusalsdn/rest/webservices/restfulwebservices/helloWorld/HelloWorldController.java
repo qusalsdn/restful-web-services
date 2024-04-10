f1a9e07b452a76c4ac9ee76be50d9af1895cf565
변민우 <yja05092@gmail.com>
2024-04-10 13:34:15 +0900

    Update HelloWorldController.java

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,7 +1,6 @@
 package com.qusalsdn.rest.webservices.restfulwebservices.helloWorld;
 
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
