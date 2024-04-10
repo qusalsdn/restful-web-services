257273cf252de4f1021af4dcda2907d13be60c46
변민우 <yja05092@gmail.com>
2024-04-10 17:53:26 +0900

    Spring Security로 Spring Boot REST API 보호

--- src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
+++ src/main/java/com/qusalsdn/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -11,10 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()); // 모든 요청이 인증을 받는다.
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // 모든 요청이 인증을 받지 않는다.
+//        코드에 return http.build();만 남겨두면 Spring Security를 비활성화 하게 된다.
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()); // 모든 요청이 인증을 받는다.
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // 모든 요청이 인증을 받지 않는다.
         http.httpBasic(Customizer.withDefaults()); // 로그인 웹 페이지가 아닌 단순 팝업창으로 인증을 요청하게 한다.
-        http.csrf(AbstractHttpConfigurer::disable); // csrf 비활성화
+        http.csrf(AbstractHttpConfigurer::disable); // POST, PUT 메소드를 사용하려면 csrf를 비활성화 해야한다.
         return http.build();
     }
 }
